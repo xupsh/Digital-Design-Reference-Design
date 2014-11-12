7df593f63f62351129c9237e377cccbd5da1dd2c
unknown <haoxingc@XSHJOSHUAL21.xlnx.xilinx.com>
2014-11-12 18:32:48 +0800

    11/12/2014 18:32

--- Projects/Digital_Logic_Cource/digital_clock_ip/readme.txt
+++ Projects/Digital_Logic_Cource/digital_clock_ip/readme.txt
@@ -8,6 +8,6 @@ step2:power on Basys3, download the B3_DigitalClock_download.bit;
 
 How to build the demo:
 step1:Open vivado 2014.2;
-step2:in the tcl console, use cd command to into the current tcl file path.For example, if tcl file is in F:/project/X.tcl, you should use this command: cd f:/project;
+step2:in the tcl console, use cd command to into the current tcl file path.For example, if tcl file is in F:/project/X.tcl, you should use this command: cd f:/project. In this project, the tcl file to run is clock_project.tcl;
 step3:Then use source command to run tcl.For example,if tcl file is in F:/project/X.tcl, you can just use command: source ./X.tcl;
 step4:Wait for a minute ,then the project is built.
--- Projects/Digital_Logic_Cource/digital_clock_ip/src/Tcl/clock_project.tcl
+++ Projects/Digital_Logic_Cource/digital_clock_ip/src/Tcl/clock_project.tcl
@@ -1,9 +1,9 @@
 set prj_name "digital_clock_ip"
-set prj_path "./prj"
+set prj_path "../prj"
 
 create_project $prj_name $prj_path -part xc7a35tcpg236-1 -force
 
-set_property ip_repo_paths ./IP_Catalog [current_fileset]
+set_property ip_repo_paths ../IP_Catalog [current_fileset]
 update_ip_catalog
 
 create_bd_design "clock"
@@ -13,7 +13,7 @@ generate_target {synthesis implementation} [get_files $prj_path/$prj_name.srcs/s
 
 make_wrapper -files [get_files $prj_path/$prj_name.srcs/sources_1/bd/clock/clock.bd] -top
 import_files -force -norecurse -fileset sources_1 $prj_path/$prj_name.srcs/sources_1/bd/clock/hdl/clock_wrapper.v
-import_files -force -norecurse -fileset constrs_1 ./constraint/clock_wrapper.xdc
+import_files -force -norecurse -fileset constrs_1 ../constraint/clock_wrapper.xdc
 set_property top clock_wrapper [current_fileset]
 
 
